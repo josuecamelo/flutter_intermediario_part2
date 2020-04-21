e3f36c61850d2d28a8eaa545303c9e1adc6dd53c
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2020-04-21 16:59:32 -0300

    Alertas com Dialog

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -67,7 +67,7 @@ class HomePage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
               BlueButton("Snack", onPressed: () => _onClickSnack(context)),
-              BlueButton("Dialog", onPressed: () => _onClickDialog),
+              BlueButton("Dialog", onPressed: () => _onClickDialog(context)),
               BlueButton("Toast", onPressed: () => _onClickToast)
             ],
           )
@@ -96,7 +96,35 @@ class HomePage extends StatelessWidget {
     ));
   }
 
-  _onClickDialog() {}
+  _onClickDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return WillPopScope(
+          onWillPop: () async => false,
+          child: AlertDialog(
+            title: Text("Flutter é muito legal"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Cancelar"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              FlatButton(
+                child: Text("OK"),
+                onPressed: () {
+                  Navigator.pop(context);
+                  print("OK !!!");
+                },
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
 
   _onClickToast() {}
 
